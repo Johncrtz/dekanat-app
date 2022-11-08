63ff18e04c88ca4b53da95ef9776ed3b74ba96e7
John <john-hildenbrand@gmx.de>
2022-11-08 17:31:03 +0100

    Test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,12 @@ main()
  * running and listen for requests. testtest
  */
 
+ async function main() {
+    await waitForDatabase().catch(e => crash<Core>(e))
+    const devMode = process.argv.includes("dev") // what could go wrong?
+    const events: EventSystem = new EventSystem(devMode) // flag sets debug mode
+    await Core.create(PLUGIN_PATHS, events).catch(e => crash<Core>(e))
+}
 
 async function waitForDatabase() {
     let connected = false
