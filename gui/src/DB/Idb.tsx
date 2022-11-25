f5c175b7eb39c368ac7ec66a4e7c72389034123a
Ajay Bhatia <ajbhatia@ucsc.edu>
2022-11-25 04:27:09 +0100

    changed JSON DB objects

--- gui/src/DB/Idb.tsx
+++ gui/src/DB/Idb.tsx
@@ -1,4 +1,4 @@
-export interface Datum {
+interface Datum {
     "Values" : String[]
     "Required": boolean
 }
@@ -29,35 +29,39 @@ export interface Unterlagenkomplett {
 export interface StatusTodo {
     "Values" : String[]
     "Required": boolean
-}export interface Entscheidung {
-    "Values" : String[]
-    "Required": boolean
-}export interface Auflage1 {
-    "Values" : String[]
-    "Required": boolean
-}export interface Auflage2 {
-    "Values" : String[]
-    "Required": boolean
-}export interface Auflage3 {
+}
+export interface Entscheidung {
     "Values" : String[]
     "Required": boolean
 }
-    export interface Table {
-        "ID": number;
-        "Datum": Datum[];
-        "Name": Name[];
-        "Vorname": Vorname[];
-        "Interdisziplinär?": Interdisziplinär[]
-        "Betreuer Fakultätsmitglied": BetreuerFakultätsmitglied[];
-        "Falltyp": Falltyp[];
-        "Unterlagen komplett?": Unterlagenkomplett[];
-        "Status / Todo": StatusTodo[];
-        "Entscheidung": Entscheidung[];
-        "Auflage 1": Auflage1[];
-        "Auflage 2": Auflage2[];
-        "Auflage 3": Auflage3[];
-        "Kommentar": string;
-    }
+export interface Auflage1 {
+    "Values" : String[]
+    "Required": boolean
+}
+export interface Auflage2 {
+    "Values" : String[]
+    "Required": boolean
+}
+export interface Auflage3 {
+    "Values" : String[]
+    "Required": boolean
+}
+export interface Table {
+    "ID": number;
+    "Datum": Datum[];
+    "Name": Name[];
+    "Vorname": Vorname[];
+    "Interdisziplinär?": Interdisziplinär[]
+    "Betreuer Fakultätsmitglied": BetreuerFakultätsmitglied[];
+    "Falltyp": Falltyp[];
+    "Unterlagen komplett?": Unterlagenkomplett[];
+    "Status / Todo": StatusTodo[];
+    "Entscheidung": Entscheidung[];
+    "Auflage 1": Auflage1[];
+    "Auflage 2": Auflage2[];
+    "Auflage 3": Auflage3[];
+    "Kommentar": string;
+}
 
-    export default Table
+export default Table
 
---
+++ gui/src/DB/Idbex.tsx
@@ -0,0 +1,23 @@
+interface Question {
+    "Question Number": Number,
+    "Question": String,
+    "Question Type": String, 
+    "Question Description": String,
+    "isRequired": Boolean,
+    "Table ID": String,
+    "Selects": String[]
+}
+
+interface Page {
+    "Page Title": String
+    "Page Description": String
+    "Questions": Question[]
+}
+
+export interface Form {
+    "Form Title": String,
+    "Form Description": String,
+    "Page Count": Number,
+    "__comment": String,
+    "Pages": Page[]
+}
